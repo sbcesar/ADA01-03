f83323d052e2fa93e54b42e579df212a4a9a3ebf
sbcesar <sbcesar@iesrafaelalberti.es>
2024-10-08 12:54:32 +0200

    seguir haciendo la funcion filereader

---
+++ src/main/kotlin/Employee.kt
@@ -0,0 +1,8 @@
+package org.example
+
+data class Employee(
+    val ID: Int,
+    val surname: String,
+    val department: String,
+    val salary: Double
+)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,41 @@
 package org.example
 
-fun fileReader() {
+import java.nio.file.Files
+import java.nio.file.Path
 
+/*
+- Funcion leer fichero (cada linea representa un empleado y separados por coma)
+- Funcion crear XML (usar DOM)
+- Funcion modificar que permita modificar el salario en funcion del ID
+- Funcion leer XML que obtiene los datos del XML y la muestra por la terminal (ID: 101, Apellido: García, Departamento: Recursos Humanos, Salario: 3000)
+
+QUE DEBES USAR
+---------------
+clases Path, Files, BufferedReader
+bibliotecas javax.xml.* org.w3c.*
+manejo de excepciones
+cerrar flujos
+ */
+
+fun fileReader(employeesFile: Path): List<Employee> {
+    val employeeList = mutableListOf<Employee>()
+
+    var isFirstLine = true
+
+    val bufferedReader = Files.newBufferedReader(employeesFile)
+    bufferedReader.use { br ->
+        br.forEachLine { line ->
+            if (isFirstLine) {
+                line.split(",")
+                isFirstLine = false
+            } else {
+                val employeeInfo = line.split(",")
+                for (i in employeeInfo.indices) {
+                    val employee = Employee(employeeInfo[i])
+                }
+            }
+        }
+    }
 }
 
 fun main() {
